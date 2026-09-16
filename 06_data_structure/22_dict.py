536a847a8b5522e9c16739127f25277b29b14203
Ganesh Dabhade <158286559+Ganeshd0003@users.noreply.github.com>
2026-09-16 10:25:22 +0530

    Data structure completed

--- 06_data_structure/22_dict.py
+++ 06_data_structure/22_dict.py
@@ -1,6 +1,3 @@
-dictionary = {'name': 'Ganesh', 'age': 20, 'role': 'data engineer'}
-print(dictionary)
-
 # Values can be duplicated
 dictt = {'a': 1, 'b': 1, 'c': 1}
 print(dictt)
@@ -9,3 +6,20 @@ print(dictt)
 # If a key is repeated, the last value overwrites the previous value.
 dicttt = {'a': 10, 'a': 'something', 'a': 'nothing'}
 print(dicttt)
+
+user = {'name': 'Ganesh', 'age': 20, 'role': 'data engineer'}
+print(user)
+print(user.get("name"))
+
+print("age" in user)
+
+print()
+
+
+for u in user:
+    print(u, user[u])
+
+print()
+
+for key, value in user.items():
+    print(key, value)
